df98106592999006f67601e9b47387a71a49aa5f
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-04 17:04:26 +0300

    PWFPD Complete
    Complete but not tested.

--- DSA-Recursion/PossibleWordsFromPhoneDigits.cpp
+++ DSA-Recursion/PossibleWordsFromPhoneDigits.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #include <vector>
 #include <string>
-#include "Permutation.cpp"
+#include "Permutation2.cpp"
 
 using namespace std;
 
@@ -16,7 +16,9 @@ private:
                         {"7", "PQRS"},
                         {"8", "TUV"},
                         {"9", "WXYZ"},
-                        {"0", "0"},}
+                        {"0", "0"},};
+    vector<string> returnValue;-
+    Permutation* perm = new Permutation();
 public:
     //Function to find list of all words possible by pressing given numbers.
     vector<string> possibleWords(int a[], int N){
@@ -24,6 +26,37 @@ public:
         string digitsPressed = "";
         for(int i = 0; i < N; i++){
             digitsPressed += a[i];
+        }
+        //find all permutations of the digits
+        perm->find_permutation(digitsPressed);
+        vector<string> allPermutations = perm->getAllPermutations();
+
+        for(size_t i = 0; i < allPermutations.size(); i++){
+            string currStr = allPermutations.at(i);
+            comb(currStr);
+        }
+
+
+
+
+        delete this->perm;
+        return this->returnValue;
+    }
+
+    void comb(string originalString){
+        comb('', "", originalString.length(), 0, originalString);
+    }
+    /*
+    * n is the length of originalString
+    */
+    void comb(const char& ch, string currString, const int& n, int index, string originalString){
+        if(currString.length() == n){
+            //base case, add the currString to the results
+            this->returnValue.push_back(currString);
+            return;
+        }
+        for(int i = index; i < n; i++){
+            comb(originalString.at(i), currString + ch, n, index + 1, originalString);
         }
 
     }

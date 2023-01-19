a20d4da1918e44f233caa612edb5403827e1510f
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-19 13:45:58 +0300

    TestVicoba implemented
    TestVicoba has been implemented

--- TestVicoba.cpp
+++ TestVicoba.cpp
@@ -1,1 +1,33 @@
+#include "VicobaAccount.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    VicobaAccount acc1(50000, 4, "Hurdaciim");
+    VicobaAccount* acc2 = new VicobaAccount(103000, 8, "Yaylacı");
+    VicobaAccount acc3(35000, 2, "Zeybek");
+
+    cout << acc1.toString() << endl;
+     cout << acc2.toString() << endl;
+      cout << acc3.toString() << endl;
+
+    acc2->setCustomerName("Yaylamin Yiğidi");
+    acc2->setLoanDuration(10);
+    cout << acc3.getCustomerName() <<"'s debt is " << acc3.getCurrentDebt() << endl;
+    cout << acc3.getCustomerName << "'s debt is " << acc3.getCurrentDebt() << endl;
+    acc3.setAmountBorrowed(45000);
+    VicobaAccount::setAnnualInterestRate(1.1);
+    cout << acc1.getCustomerName << "'s debt is " << acc1.getCurrentDebt() << endl;
+
+    cout << acc1.toString() << endl;
+     cout << acc2.toString() << endl;
+      cout << acc3.toString() << endl;
+
+
+
+    return 0;
+}
--- VicobaAccount.h
+++ VicobaAccount.h
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 #ifndef VICOBA_ACCOUNT_H
 #define VICOBA_ACCOUNT_H
+
 class VicobaAccount{
 private:
     double amountBorrowed;
     static double annualInterestRate = 1.2;
     int loanDuration; // in years
     string customerName;
-    int customerID; // generate new ID each time
+    const int customerID; // generate new ID each time
     static vector<int> usedIDS;
 public:
     VicobaAccount(double ab,int ld, string cn);
+    ~VicobaAccount();
     static void setAnnualInterestRate(double);
-    double currentDebt();
+    double getCurrentDebt() const;
+
     double getAmountBorrowed() const;
+    static double getAnnualInterestRate() const;
+    int getLoanDuration() const;
+    string getCustomerName() const;
+
+    void setAmountBorrowed(double);
+    void setLoanDuration(int);
+    void setCustomerName(string);
+    string toString() const;
 };
 
 

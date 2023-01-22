7787d83ecbeba3522691f7b8a57a48681178f62c
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-22 20:39:45 +0300

    binarySearch implementation
    Recursive implementation

--- DSA-Array/GeekArray.cpp
+++ DSA-Array/GeekArray.cpp
@@ -57,5 +57,22 @@ public:
 
         return result;
     }
+    int binarySearch(int arr[], int low, int high, const int& x){
 
+        if(low > high){
+            //base case
+            return - 1;
+        }
+        int mid = high + low / 2;
+        if(arr[mid] == x){
+            return mid;
+            //base case
+        }
+        else if(arr[mid] > x){
+            return binarySearch(arr,low, mid - 1, x);
+        }
+        else{
+            return binarySearch;(arr, mid + 1, high, x);
+        }
+    }
 };

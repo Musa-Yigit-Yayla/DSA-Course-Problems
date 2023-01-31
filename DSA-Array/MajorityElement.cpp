3b1aae25e30eb61804e6d3491c30264c0b7f29d2
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-31 21:40:15 +0300

    Change again
    Again changed

--- DSA-Array/MajorityElement.cpp
+++ DSA-Array/MajorityElement.cpp
@@ -61,10 +61,11 @@ class Solution{
             }
             else if(arr[mid] > X){
                 //search for right half
-                low = mid + 1;
+
+                high = mid - 1;
             }
             else{
-                high = mid - 1;
+                low = mid + 1;
             }
         }
         return - index;

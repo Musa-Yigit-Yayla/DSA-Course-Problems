8d5c6b09247ceccf8f1670a6647307631281809d
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-31 21:33:31 +0300

    Logic change
    Logic change

--- DSA-Array/MajorityElement.cpp
+++ DSA-Array/MajorityElement.cpp
@@ -59,13 +59,12 @@ class Solution{
             }
             else if(arr[mid] > X){
                 //search for right half
-                low = mid + 1;
+                low = mid - 1;
             }
             else{
-                high = mid - 1;
+                high = mid + 1;
             }
         }
         return - 1;
-
     }
 };

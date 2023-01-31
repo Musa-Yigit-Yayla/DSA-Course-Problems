b456ef41fe4fb4e634e147b314c05ec5f080136c
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-01-31 21:28:34 +0300

    leftIndex added
    Binary search implementation of leftIndex function added.

--- DSA-Array/MajorityElement.cpp
+++ DSA-Array/MajorityElement.cpp
@@ -46,4 +46,26 @@ class Solution{
         }
 
     }
+    int leftIndex(int N, int arr[], int X){
+
+        // Your code here
+        int low = 0, high = N - 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == X && ((mid != 0 && arr[mid - 1] != X) || mid == 0)){
+                return mid;
+            }
+            else if(arr[mid] > X){
+                //search for right half
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return - 1;
+
+    }
 };

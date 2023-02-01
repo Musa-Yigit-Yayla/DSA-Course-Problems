a971ed30310d9a711b093ccba9bb2c018e6b2af3
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-01 17:42:11 +0300

    floorSqrt function added
    floorSqrt function implemented.

--- DSA-Array/SmallestPositiveMissing.cpp
+++ DSA-Array/SmallestPositiveMissing.cpp
@@ -62,4 +62,28 @@ class SmallestPositiveMissing{
         n1 = n2;
         n2 = &temp;
     }
+    class Solution{
+  public:
+    long long int floorSqrt(long long int x)
+    {
+        // Your code goes here
+        if(x == 0 || x == 1){
+            return x;
+        }
+        int l = 1, r = x / 2;
+        int ans;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(pow(x, 2) >= mid){
+                ans = mid;
+                l = mid + 1;
+            }
+            else if(pow(mid, 2) > x){
+                //search for the left half
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
 };

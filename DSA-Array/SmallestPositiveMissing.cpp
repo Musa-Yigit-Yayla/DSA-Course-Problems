5097c7abed7830ee224d03f97341c3c664a828c4
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-01 17:52:36 +0300

    floorSqrt implementation changed
    implementation changed slightly

--- DSA-Array/SmallestPositiveMissing.cpp
+++ DSA-Array/SmallestPositiveMissing.cpp
@@ -62,28 +62,31 @@ class SmallestPositiveMissing{
         n1 = n2;
         n2 = &temp;
     }
-    class Solution{
-  public:
-    long long int floorSqrt(long long int x)
-    {
-        // Your code goes here
-        if(x == 0 || x == 1){
-            return x;
-        }
-        int l = 1, r = x / 2;
-        int ans;
-        while(l <= r){
-            int mid = (l + r) / 2;
-            if(pow(x, 2) >= mid){
-                ans = mid;
-                l = mid + 1;
-            }
-            else if(pow(mid, 2) > x){
-                //search for the left half
-                r = mid - 1;
-            }
-        }
-        return ans;
-    }
-};
+    int floorSqrt(int x)
+{
+	// Base cases
+	if (x == 0 || x == 1)
+		return x;
+
+	// Do Binary Search for floor(sqrt(x))
+	int start = 1, end = x / 2, ans;
+	while (start <= end) {
+		int mid = (start + end) / 2;
+
+		// If x is a perfect square
+		int sqr = mid * mid;
+		if (sqr == x)
+			return mid;
+
+
+		if (sqr <= x) {
+			start = mid + 1;
+			ans = mid;
+		}
+		else // If mid*mid is greater than x
+			end = mid - 1;
+	}
+	return floor(sqrt(x));
+}
+
 };

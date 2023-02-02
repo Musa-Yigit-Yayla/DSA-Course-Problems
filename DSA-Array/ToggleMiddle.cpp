c6918e03059de7c1408c78df9e468793f6a102a4
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-02 18:55:18 +0300

    ToggleMid changed
    changed slightly

--- DSA-Array/ToggleMiddle.cpp
+++ DSA-Array/ToggleMiddle.cpp
@@ -1,68 +1,65 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int number;
-string binaryNumber;
+//int arr[5][4] = {{1, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 1}};
 
-int toggleMid(){
-    bool isEvenLength = (binaryNumber.size() % 2 == 0);
-    if(isEvenLength){
-        char ch1 = binaryNumber.at(binaryNumber.size() / 2);
-        char ch2 = binaryNumber.at((binaryNumber.size() / 2) - 1);
+int* arr;
+int noOfColumns, noOfRows;
+void fun2(int, int);
+void function( int rows, int columns){
+    //check each row for whether that row contains one, if it does call fun2 on that row
+    for(int k = 0; k < rows; k++){
+        for(int j = 0; j < columns; j++){
+            if(*(arr + (k * noOfColumns + j) * sizeof(int)) == 1){
+                fun2(k, columns);
+                break;
+            }
+        }
+    }
 
-        if(ch1 == '0'){
-            binaryNumber.at(binaryNumber.size() / 2) = '1';
-        }
-        else{
-            binaryNumber.at(binaryNumber.size() / 2) = '0';
-        }
-        if(ch2 == '0'){
-            binaryNumber.at((binaryNumber.size() / 2) - 1) = '1';
-        }
-        else{
-            binaryNumber.at((binaryNumber.size() / 2) - 1) = '0';
-        }
-    }
-    else{
-        char ch1 = binaryNumber.at(binaryNumber.size() / 2);
-        if(ch1 == '0'){
-            binaryNumber.at(binaryNumber.size() / 2) = '1';
-        }
-        else{
-            binaryNumber.at(binaryNumber.size() / 2) = '0';
-        }
-    }
 }
-void convertToBinary(int n)
-{
-    if (n / 2 != 0) {
-        convertToBinary(n / 2);
+void fun2( int rowIndex, int columns){
+    for(int j = 0; j < columns; j++){
+        *(arr + (rowIndex * noOfColumns + j) * sizeof(int)) = 1;
+
     }
-    binaryNumber += (n % 2);
-}
-int convertToDecimal(){
-    int binary = stoi(binaryNumber);
-    cout << binary;
-    int sum = 0;
-    for(int i = 0; i < binaryNumber.size(); i++){
-        int currDigit = binary % 10;
-        sum += currDigit;
-        binary /= 10;
-    }
-    return sum;
 }
 int main() {
-    int T;
-    cin >> T;
-    while(T > 0){
-        cin >> number;
-        convertToBinary(number);
-        toggleMid();
-        convertToDecimal();
-        //cout << convertToDecimal() << endl;
-        T--;
-    }
 	//code
+	int T;
+	int m, n; // m rows, n columns
+	cin >> T;
+
+	while(T >= 0){
+	    cin >> m;
+	    cin >> n;
+	    int a[m][n];
+	    arr = &a[0][0];
+	    noOfRows = m;
+	    noOfColumns = n;
+	    int nextValue;
+	    for(int j = 0; j < m; j++){
+
+
+	        for(int i = 0; i < n; i++){
+	            cin >> nextValue;
+	            //place in the array
+	            a[j][i] = nextValue;
+	            //cout << nextValue;
+	        }
+	        //cout << endl;
+	    }
+	    function(m, n);
+	    T--;
+	}
+	//function(m, n);
+	for(int i = 0; i < noOfRows; i++){
+	    //cout << "hey2";
+	    for(int j = 0; j < noOfColumns; j++){
+	        //cout << "hey1";
+	        cout << *(arr + (i * noOfColumns + j) * sizeof(int));
+	    }
+	    cout << endl;
+	}
 	return 0;
 }

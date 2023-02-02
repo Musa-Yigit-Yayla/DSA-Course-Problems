4fe21874247ddc2b203096fca8efe4ca63aec126
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-02 12:08:09 +0300

    PeakElement solved!
    Solved thoroughly, passed all testcases.

--- DSA-Array/PeakElement.cpp
+++ DSA-Array/PeakElement.cpp
@@ -2,15 +2,21 @@
 
 using namespace std;
 
-class Solution{
+class PeakElement{
 public:
     int peakElement(int arr[], int n){
        // Your code here
        int l = 0, r = n - 1;
+       if(arr[l] >= arr[l + 1]){
+           return l;
+       }
+       else if(arr[r] >= arr[r - 1]){
+           return r;
+       }
        while(l <= r){
         int mid = (l + r) / 2;
-        if((mid == 0 && arr[mid + 1] <= arr[mid]) || (arr[mid - 1] <= arr[mid] &&  arr[mid] <= arr[mid + 1]) || (arr[mid - 1] <= arr[mid] && mid == n - 1)){
-            return arr[mid];
+        if((mid == 0 && arr[mid + 1] <= arr[mid]) || ((mid != 0 && arr[mid - 1] <= arr[mid]) &&  (mid != n - 1 && arr[mid] <= arr[mid + 1])) || (arr[mid - 1] <= arr[mid] && mid == n - 1)){
+            return mid;
         }
         else if( arr[mid] <= arr[mid - 1]  ){
             //search the left side
@@ -19,7 +25,7 @@ public:
         else{
          l = mid + 1;
         }
-}
-}
-}
+      }
+    }
 };
+

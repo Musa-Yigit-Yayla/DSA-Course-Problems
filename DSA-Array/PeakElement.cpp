4b176f54e79c5dac52d3bdb0cafdd37be44bc1b2
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-05 12:19:59 +0300

    findRepeating function added
    findRepeatingFunction implementation added, all implementation and design belongs to me.

--- DSA-Array/PeakElement.cpp
+++ DSA-Array/PeakElement.cpp
@@ -27,5 +27,36 @@ public:
         }
       }
     }
+    pair<long, long> findRepeating(long *arr, int n){
+        //code here
+        int low = 0, high = n - 1;
+        int mid;
+        int repeatingElt = - 1;
+        int noOfOccurences = n - (arr[n - 1] - arr[0]); // taken from the hint of the problem
+        while(low <= high){
+            mid = (low + high / 2);
+            if((mid != 0 && mid != n - 1 && (arr[mid] == arr[mid - 1] || arr[mid] == arr[mid + 1]))){
+            //|| ( mid == 0 && arr[mid + 1] == arr[mid]) || (mid == n - 1 && arr[mid - 1] == arr[mid])){
+                repeatingElt = arr[mid];
+            }
+            else if(arr[mid] >= mid + arr[0]){ //condition taken from the hint of the problem
+            //evaluate the right side if this is true
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        if(repeatingElt == -1){
+            //no match has been found
+            pair<long, long> newPair(-1, -1);
+            return newPair;
+        }
+        else{
+            pair<long, long> newPair(repeatingElt, noOfOccurences);
+            return newPair;
+        }
+
+    }
 };
 

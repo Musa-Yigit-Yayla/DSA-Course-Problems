01a9bb076fe1fdbfcd32e596d66047b6dfeb3bd0
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-06 11:32:38 +0300

    AllocateMinPages added
    AllocateMinPages solution added, fails some testcases needs improvement.

---
+++ DSA-SearchingSorting/AllocateMinPages.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to find minimum number of pages.
+    int findPages(int A[], int N, int M)
+    {
+        //code here
+        //sum the pages
+        int sum = 0;
+        int minPages = A[0]; // minimum of the pages
+        for(int i = 0; i < N; i++){
+            sum += A[i];
+            if(A[i] < minPages){
+                minPages = A[i];
+            }
+        }
+
+        int start = 0, end = sum;
+        int result = -1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(this->isValid(A, N, M, mid)){
+                result = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+    //function to check whether curr_min is feasible or not
+    bool isValid(int A[], int N, int M, int curr_min){
+        int studentsRequired = 1;
+        int curr_sum = 0;
+
+        for(int i = 0; i < N; i++){
+            curr_sum += A[i];
+            if(curr_sum > curr_min){
+                studentsRequired++;
+            }
+            if(studentsRequired > M){
+                return false;
+            }
+        }
+        return true;
+    }
+};

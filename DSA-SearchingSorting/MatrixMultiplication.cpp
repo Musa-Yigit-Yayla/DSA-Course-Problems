d4d1dcb829f3532a99b8ab7ea2e06837ee112a2c
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-15 11:43:53 +0300

    Pushing matrix question
    .

---
+++ DSA-SearchingSorting/MatrixMultiplication.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+    public:
+    //Function to multiply two matrices.
+    vector<vector<int>> multiplyMatrix( const vector<vector<int>>& A, const vector<vector<int>>& B){
+        // code here
+
+        vector<vector<int>> result;
+
+
+        int noOfColumnsInA = A.at(0).size();
+        int noOfRowsInB = B.size();
+
+        if(noOfColumnsInA == noOfRowsInB){
+            for(int i = 0; i < A.size(); i++){
+                vector<int> currRow;
+                for(int j = 0; j < B.at(0).size(); j++){
+                    currRow.push_back(calculateElt(A, B, i, j));
+                }
+                result.push_back(currRow);
+            }
+        }
+        return result;
+    }
+
+    //Utility function to calculate an individual element on the matrix
+    int calculateElt(const vector<vector<int>>& A, const vector<vector<int>>& B, int i, int j){
+        int sum = 0;
+        for(int k = 0; k < A.size(); k++){
+            sum += (A.at(i).at(k) * B.at(k).at(j));
+        }
+        return sum;
+    }
+    //Utility function to print any given matrix of integers
+    void printMatrix(const vector<vector<int>> a){
+        for(int i = 0; i < a.size(); i++){
+            for(int j = 0; j < a.at(i).size(); j++){
+                cout << a.at(i).at(j) << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(){
+
+    Solution sol;
+    vector<vector<int>> a;
+    vector<vector<int>> b;
+
+    int aRowSize , aColumnSize;
+    int bRowSize, bColumnSize;
+
+    cout << "Enter A's dimensions respectively: ";
+    cin >> aRowSize >> aColumnSize;
+    cout << "\nEnter B's dimensions respectively: ";
+    cin >> bRowSize; bColumnSize;
+
+    cout << "\nEnter elements of A:"
+    for(int i = 0; i < aRowSize; i++){
+        vector<int> currRow
+        for(int j = 0; j < aColumnSize; j++){
+            int value;
+            cin >> value;
+            currRow.push_back(value);
+        }
+        a.push_back(currRow);
+        cout << "\n";
+    }
+    cout << "\nEnter elements of B:"
+    for(int i = 0; i < aRowSize; i++){
+        vector<int> currRow
+        for(int j = 0; j < aColumnSize; j++){
+            int value;
+            cin >> value;
+            currRow.push_back(value);
+        }
+        b.push_back(currRow);
+        cout << "\n";
+    }
+    vector<vector<int>> result = sol.multiplyMatrix(a, b);
+    sol.printMatrix(result);
+
+    return 0;
+}

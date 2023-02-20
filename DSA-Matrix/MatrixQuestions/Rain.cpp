97800eabbdcc94974e233f39b91b5a9fc7714107
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-20 09:53:43 +0300

    Rain program implemented
    Simple program representing rain drops

---
+++ DSA-Matrix/MatrixQuestions/Rain.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+string generateLine();
+
+int main(){
+
+    long long counter = 0;
+    while(counter < 100000000){
+        string s = generateLine();
+        cout << s << endl;
+        counter++;
+    }
+    return 0;
+}
+string generateLine(){
+    string s = "";
+    for(int i = 0; i < 100; i++){
+        int value = rand() % 2;
+        if(value == 0){
+            s += " ";
+        }
+        else{
+            s += "1";
+        }
+    }
+    return s;
+}

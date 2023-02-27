51f3315ba5bab79513daad8e8eb2923926b0c830
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-27 12:32:28 +0300

    QuadraticProbing added

---
+++ DSA-Hashing/QuadraticProbing.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+
+class QuadraticProbing{
+  public:
+    //Function to fill the array elements into a hash table
+    //using Quadratic Probing to handle collisions.
+    void QuadraticProbing(vector<int>& hash, int hashSize, int arr[], int N)
+    {
+        //Your code here
+        //set the each value of hash into int min
+        for(int i = 0; i < hashSize; i++){
+            hash.at(i) = -1;
+        }
+        for(int i = 0; i < N; i++){
+            int curr = arr[i];
+            int hashIndex = (hashSize % N);
+
+            if(hash.at(hashIndex) == -1){
+                //insert
+                hash.insert(hash.begin() + hashIndex, curr);
+            }
+            else{
+                //use the quadratic probing
+                int l = 1;
+                while(hash.at(hashIndex) == -1){
+                    hashIndex = ((hashSize % N) + (static_cast<int>(pow(l, 2)))) % N;
+                    if(hash.at(hashIndex) == INT_MIN){
+                        hash.insert(hash.begin() + hashIndex, curr);
+                    }
+                    l++;
+                }
+            }
+        }
+    }
+
+
+};
+

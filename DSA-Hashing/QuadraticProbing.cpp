7c431d241b809d95f589f6fcb93662cde7f85ab6
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-02-28 14:00:14 +0300

    Some alterations on QuadraticProbing

--- DSA-Hashing/QuadraticProbing.cpp
+++ DSA-Hashing/QuadraticProbing.cpp
@@ -1,38 +1,39 @@
 #include <cmath>
+#include <vector>
+
+using namespace std;
 
 class QuadraticProbing{
   public:
     //Function to fill the array elements into a hash table
     //using Quadratic Probing to handle collisions.
-    void QuadraticProbing(vector<int>& hash, int hashSize, int arr[], int N)
-    {
+    void QuadraticProbing(vector<int>& hashVector, int hashSize, int arr[], int N){
         //Your code here
         //set the each value of hash into int min
         for(int i = 0; i < hashSize; i++){
-            hash.at(i) = -1;
+            hashVector.at(i) = -1;
         }
         for(int i = 0; i < N; i++){
             int curr = arr[i];
             int hashIndex = (hashSize % N);
 
-            if(hash.at(hashIndex) == -1){
+            if(hashVector.at(hashIndex) == -1){
                 //insert
-                hash.insert(hash.begin() + hashIndex, curr);
+                hashVector.insert(hashVector.begin() + hashIndex, curr);
             }
             else{
                 //use the quadratic probing
                 int l = 1;
-                while(hash.at(hashIndex) != -1){
+                while(hashVector.at(hashIndex) != -1){
                     hashIndex = ((hashSize % N) + (static_cast<int>(pow(l, 2)))) % N;
-                    if(hash.at(hashIndex) == -1){
-                        hash.insert(hash.begin() + hashIndex, curr);
+                    if(hashVector.at(hashIndex) == -1){
+                        hashVector.insert(hashVector.begin() + hashIndex, curr);
                     }
                     l++;
                 }
             }
         }
     }
-
 
 };
 
--- DSA-Hashing/main.cpp
+++ DSA-Hashing/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <vector>
+#include "QuadraticProbing.cpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    QuadraticProbing qp;
+    vector<int> hashVector;
+    int arr[] = {21, 10, 32, 43};
+    int arrLength = 4;
+    qp.QuadraticProbe(hashVector, 11, arr, arrLength);
+
     return 0;
 }

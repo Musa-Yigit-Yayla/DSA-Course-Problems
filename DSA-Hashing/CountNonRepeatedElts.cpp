89e8d8570db1e4a5efd44e75c79a908b6d02da83
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-05 15:52:50 +0300

    Solved printNonRepeated
    Solved passed most testcases but exceeds time limit, needs optimization.

--- DSA-Hashing/CountNonRepeatedElts.cpp
+++ DSA-Hashing/CountNonRepeatedElts.cpp
@@ -31,4 +31,41 @@ class CountNonRepeatedElts{
         }
         return counter;
     }
+    // arr[]: input array
+    // n: size of array
+    //Function to return non-repeated elements in the array.
+    vector<int> printNonRepeated(int arr[], int n){
+        //Your code here
+        unordered_set<int> set;
+        vector<int> alreadyFound; // keeps track of the already found and checked elements inside the loop
+        vector<int> result;
+        vector<int> originalArr(arr, arr + n);
+
+        //add all the elements into the set, notice multiple elements will only be added once to the set
+        for(int i = 0; i < n; i++){
+            set.insert(arr[i]);
+        }
+        //Traverse the set, if the current element occurs once in the given array, then we add it to the result
+        for(auto it: set){
+            int curr = it; // might be problematic
+            if(!(count(arr, arr + n, curr) > 1)){
+                //no multiple occurences, add it to the result
+                result.push_back(curr);
+            }
+        }
+
+        //remove the elements which are not occuring in result from the original array
+        for(int i = 0; i < n; i++){
+            int curr = arr[i];
+            if(count(result.begin(), result.end(), curr) == 0){
+                //originalArr.remove(originalArr.begin(), originalArr.end(), curr);
+                originalArr.erase(std::remove(originalArr.begin(), originalArr.end(), curr), originalArr.end());
+
+            }
+        }
+
+        //assign result to originalArr
+        result = originalArr;
+        return result;
+    }
 };

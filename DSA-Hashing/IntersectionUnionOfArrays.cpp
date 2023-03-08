9b212d2df1f18c67b7241a3e9ee5078020f7f277
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-08 13:20:44 +0300

    IntersectionOfArrays solved
    Solved completely.

--- DSA-Hashing/IntersectionUnionOfArrays.cpp
+++ DSA-Hashing/IntersectionUnionOfArrays.cpp
@@ -5,14 +5,20 @@ class IntersectionUnionOfArrays{
   public:
     // Function to return the count of the number of elements in
     // the intersection of two arrays.
-    int NumberofElementsInIntersection(int arr[], int b[], int n, int m) {
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
         // Your code goes here
         int result = 0;
-        unordered_set<int> set(b, b + m);
+        unordered_set<int> set1(a, a + n);
+        unordered_set<int> set2(b, b + m);
 
-        for(int i = 0; i < n; i++){
-            int curr = arr[i];
-            if(set.find(curr) != set.end()){
+        /*for(int i = 0; i < set2.size(); i++){
+            int curr = set2.at(i);
+            if(set1.find(curr) != set1.end()){
+                result++;
+            }
+        }*/
+        for(auto it: set2){
+            if(set1.find(it) != set1.end()){
                 result++;
             }
         }

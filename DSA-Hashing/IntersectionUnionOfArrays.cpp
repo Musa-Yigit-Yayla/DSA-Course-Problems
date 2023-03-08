3d766149d5f5cb7bf5500f194c6dc059066cf752
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-08 13:05:25 +0300

    Subtle update

--- DSA-Hashing/IntersectionUnionOfArrays.cpp
+++ DSA-Hashing/IntersectionUnionOfArrays.cpp
@@ -1,23 +1,21 @@
 
 #include <unordered_set>
 
-class Solution {
+class IntersectionUnionOfArrays{
   public:
     // Function to return the count of the number of elements in
     // the intersection of two arrays.
     int NumberofElementsInIntersection(int arr[], int b[], int n, int m) {
         // Your code goes here
         int result = 0;
-        unordered_set<int> set;
-        for(int i = 0; i < m; i++){
-            //add the b elements to our unordered_set
-            set.insert(b[i]);
-        }
+        unordered_set<int> set(b, b + m);
+
         for(int i = 0; i < n; i++){
             int curr = arr[i];
-            if(find(set.begin(), set.end(), curr) != set.end()){
+            if(set.find(curr) != set.end()){
                 result++;
             }
         }
         return result;
     }
+};

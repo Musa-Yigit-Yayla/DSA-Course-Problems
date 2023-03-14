01fb9c2f1bab022310306acabeba88ab008243c1
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-14 14:07:01 +0300

    substr method implemented

--- DSA-StringQuestions/CheckForRotations.cpp
+++ DSA-StringQuestions/CheckForRotations.cpp
@@ -2,43 +2,62 @@
 
 using namespace std;
 
-//Function to locate the occurrence of the string x in the string s.
-int strstr(string s, string x){
-     //Your code here
-     char ch = x.at(0);
-     int currCharIndex = 0;
-     int result = -1;
-     int prevResult = -1;
+class CheckForRotations{
+public:
+    //Function to locate the occurrence of the string x in the string s.
+    int strstr(string s, string x){
+        //Your code here
+        char ch = x.at(0);
+        int currCharIndex = 0;
+        int result = -1;
+        int prevResult = -1;
 
-     for(int i = 0; i < s.size() && currCharIndex <= x.size(); i++){
-         if(currCharIndex == x.size()){
-             //reset the ch, searching again
-             ch = x.at(0);
-             currCharIndex = 0;
-             result = prevResult;
-         }
-         char curr = s.at(i);
-         if(ch == curr){
-             if(curr == x.at(0)){
-                result = i; //assuming this is the beginning of our subsequence
-             }
-             if(currCharIndex != x.size() - 1){
-                ch = x.at(++currCharIndex);
-             }
-             else{
-                 //reset the currCharIndex
-                 currCharIndex = 0;
-                 ch = x.at(currCharIndex);
-                 result = prevResult;
-             }
-         }
-         else{
-             //no such subsequence reset the properties
-             currCharIndex = 0;
-             ch = x.at(currCharIndex);
-             result = prevResult;
-         }
-     }
+        for(int i = 0; i < s.size() && currCharIndex <= x.size(); i++){
+            if(currCharIndex == x.size()){
+                //reset the ch, searching again
+                ch = x.at(0);
+                currCharIndex = 0;
+                result = prevResult;
+            }
+            char curr = s.at(i);
+            if(ch == curr){
+                if(curr == x.at(0)){
+                    result = i; //assuming this is the beginning of our subsequence
+                }
+                if(currCharIndex != x.size() - 1){
+                    ch = x.at(++currCharIndex);
+                }
+                else{
+                    //reset the currCharIndex
+                    currCharIndex = 0;
+                    ch = x.at(currCharIndex);
+                    result = prevResult;
+                }
+            }
+            else{
+                //no such subsequence reset the properties
+                currCharIndex = 0;
+                ch = x.at(currCharIndex);
+                result = prevResult;
+            }
+        }
 
-     return result;
-}
+        return result;
+    }
+    //Function to return the beginning index of the first occurrence of a substring in a given string if exists
+    //Returns -1 otherwise
+    int substr(string s, string sub){
+        for(int i = 0; i <= s.size() - sub.size(); i++){
+            string currSubStr = "";
+            for(int j = i; j < (i + sub.size()) && i + (sub.size()) < s.size(); j++){
+                currSubStr += s.at(j);
+            }
+            if(currSubStr == sub){
+                return i;
+            }
+            currSubStr = "";
+        }
+
+        return -1;
+    }
+};
--- DSA-StringQuestions/main.cpp
+++ DSA-StringQuestions/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "CheckForRotations.cpp"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(){
+    CheckForRotations cfr;
+
+    string s1 = "ababaaaaaa";
+    string s2 = "abaa";
+    cout << cfr.strstr(s1, s2) << endl;
+
     return 0;
 }

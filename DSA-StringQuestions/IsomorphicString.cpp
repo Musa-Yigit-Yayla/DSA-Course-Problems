6093e825885553fbe520d93f782e32d0d4ef29da
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-14 14:42:25 +0300

    Altered implementation
    Passed 52/288 testcases but failed at some point. Needs further evaluation.

--- DSA-StringQuestions/IsomorphicString.cpp
+++ DSA-StringQuestions/IsomorphicString.cpp
@@ -15,7 +15,7 @@ public:
         this->mapValues(map1, str1);
         this->mapValues(map2, str2);
 
-        vector<int> vec1;
+        /*vector<int> vec1;
         vector<int> vec2;
 
         for(auto it: map1){
@@ -23,8 +23,34 @@ public:
         }
         for(auto it: map2){
             vec2.push_back(it.second);
+        }*/
+
+        vector<int> vec3;
+        vector<int> vec4;
+
+        for(int i = 0; i < str1.size(); i++){
+            char ch = str1.at(i);
+            int occurrence = map1.at(ch);
+            vec3.push_back(occurrence);
+        }
+        for(int i = 0; i < str2.size(); i++){
+            char ch = str2.at(i);
+            int occurrence = map2.at(ch);
+            vec4.push_back(occurrence);
         }
 
+        if(vec3.size() == vec4.size()){
+            for(int i = 0; i < vec3.size(); i++){
+                int curr3 = vec3.at(i);
+                int curr4 = vec4.at(i);
+                if(curr3 != curr4){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+        /*
         if(vec1.size() == vec2.size()){ // for optimization purposes
             for(int i = 0; i < vec1.size(); i++){
                 int curr = vec1.at(i);
@@ -34,7 +60,7 @@ public:
             }
             return true;
         }
-        return false;
+        return false;*/
     }
     void mapValues(unordered_map<char, int>& map1, const string& str1){
         for(int i = 0; i < str1.size(); i++){

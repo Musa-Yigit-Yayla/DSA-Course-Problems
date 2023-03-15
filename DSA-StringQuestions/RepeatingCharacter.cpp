659126e5b3c43179e84fda92860566ff37d26ca5
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-15 19:08:08 +0300

    NonRepeatingCharacter also implemented
    Implemented and tested, works fluently.

--- DSA-StringQuestions/RepeatingCharacter.cpp
+++ DSA-StringQuestions/RepeatingCharacter.cpp
@@ -27,4 +27,26 @@ public:
         }
         return -1;
     }
+
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string s){
+       //Your code here
+       unordered_map<char, int> map; // map to count the frequencies of our characters
+       for(int i = 0; i < s.size(); i++){
+           char ch = s.at(i);
+           if(map.find(ch) == map.end()){
+               map.insert({ch, 1});
+           }
+           else{
+               map.at(ch)++;
+           }
+       }
+       for(int i = 0; i < s.size(); i++){
+           char ch = s.at(i);
+           if(map.at(ch) == 1){
+               return ch;
+           }
+       }
+       return '$';
+    }
 };

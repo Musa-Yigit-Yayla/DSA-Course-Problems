ebf1367f58b574e8f23ba0ec57dbdd9af9cd67fa
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-17 22:58:58 +0300

    concatenatedStrings implemented
    Passed 96/310 testcases, however exceeds time limit. May need to utilize map.

--- DSA-StringQuestions/ReverseWords.cpp
+++ DSA-StringQuestions/ReverseWords.cpp
@@ -2,7 +2,7 @@
 *Program to reverse given words in a string, separated by dots
 *Musa Yiğit Yayla
 */
-class Solution{
+class ReverseWords{
 public:
     //Function to reverse words in a given string.
     string reverseWords(string s){
@@ -56,5 +56,31 @@ public:
             high--;
         }
         s = newS;
+    }
+    //Function to remove common characters and concatenate two strings.
+    string concatenatedString(string s1, string s2){
+        // Your code here
+        string initialS1 = s1;
+
+        removeChars(s1, s2);
+        removeChars(s2, initialS1);
+
+        if(s1.size() == 0 && s2.size() == 0){
+            return "-1";
+        }
+        else{
+            return s1 + s2;
+        }
+
+    }
+    void removeChars(string& s1, string s2){
+        string newS1 = "";
+        for(int i = 0; i < s1.size(); i++){
+            char ch = s1.at(i);
+            if(count(s2.begin(), s2.end(), ch) == 0){
+                newS1 += ch;
+            }
+        }
+        s1 = newS1;
     }
 };

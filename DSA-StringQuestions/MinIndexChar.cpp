6662523b376bfb1f31c6775ef61879dcec029e36
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-03-17 11:42:08 +0300

    modified method added
    Worked fine for the very first few testcases. However, gives the wrong output now. Needs further attention.

--- DSA-StringQuestions/MinIndexChar.cpp
+++ DSA-StringQuestions/MinIndexChar.cpp
@@ -36,4 +36,45 @@ class MinIndexChar{
         }
         return -1;
     }
+    //Function to find minimum number of characters which Ishaan must insert
+    //such that string doesn't have three consecutive same characters.
+    int modified (string s){
+        // Your code here
+        int consecCount = 1;
+        char prevChar = s.at(0);
+        char maxChar = prevChar;
+        int maxConsecCount = 1;
+        vector<int> maxConsecCounts;
+        bool consecCharFound = false;
+        for(int i = 1; i < s.size(); i++){
+            char ch = s.at(i);
+            if(!consecCharFound){
+                if(prevChar == ch){
+                    consecCount++;
+
+                }
+                else{
+                    if(consecCount >= 3){
+                        maxConsecCounts.push_back(consecCount);
+                    }
+                    consecCount = 1;
+                    prevChar = ch;
+                }
+                if(consecCount > maxConsecCount){
+                    maxConsecCount = consecCount;
+                }
+            }
+            if(ch > maxChar){
+                maxChar = ch;
+            }
+
+        }
+        int sum = 0;
+        for(int i = 0; i < maxConsecCounts.size(); i++){
+            int curr = maxConsecCounts.at(i);
+            cout << curr;
+            sum += ceil(curr / 2.0) - 1;
+        }
+        return sum;
+    }
 };

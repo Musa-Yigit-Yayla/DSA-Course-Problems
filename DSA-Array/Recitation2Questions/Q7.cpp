92bf9375f9f22ceb0e934357113981dddd460b3e
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-04-09 12:28:17 +0300

    splitArray solved thoroughly

--- DSA-Array/Recitation2Questions/Q7.cpp
+++ DSA-Array/Recitation2Questions/Q7.cpp
@@ -1,23 +1,35 @@
 //Split array function given in Recitation2
 //Musa Yiğit Yayla
+#include <iostream>
 
-//Assuming i1 and i2 are valid indexes ensuring a valid split of array
-//subArr1 will contain the middle elements whereas subArr2 will contain the rest
-//i2 inclusive, i2 > i1
-void splitArray(int* arr, int n, int*& subArr1, int*& subArr2, int i1, int i2){
-    delete[] subArr1; //you can delete even if it's a nullptr
-    delete[] subArr2;
+using namespace std;
+class Q7{
+public:
+    //Assuming i1 and i2 are valid indexes ensuring a valid split of array
+    //subArr1 will contain the middle elements whereas subArr2 will contain the rest
+    //i2 inclusive, i2 > i1
+    void splitArray(const int arr[], int n, int*& subArr1, int*& subArr2, int i1, int i2){
+        delete[] subArr1; //you can delete even if it's a nullptr
+        delete[] subArr2;
 
-    subArr1 = new int[i2 - i1 + 1];
-    subArr2 = new int[n - i2 + i1 - 1];
-    for(int i = i1, counter = 0; i <= i2; i++, counter++){
-        subArr1[counter] = arr[i];
-    }
-    for(int i = 0, counter; i < n; i++, counter++){
-        if(i >= i1 || i <= i2){
-            counter--;
-            continue;
+        subArr1 = new int[i2 - i1 + 1];
+        subArr2 = new int[n - i2 + i1 - 1];
+        for(int i = i1, counter = 0; i <= i2; i++, counter++){
+            subArr1[counter] = arr[i];
         }
-        subArr2[counter] = arr[i];
+        for(int i = 0, counter = 0; i < n; i++, counter++){
+            if(i >= i1 && i <= i2){
+                counter--;
+                continue;
+            }
+            subArr2[counter] = arr[i];
+        }
     }
-}
+
+    void displayArr(const int* arr, int n){
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
--- DSA-Array/Recitation2Questions/main.cpp
+++ DSA-Array/Recitation2Questions/main.cpp
@@ -1,9 +1,28 @@
-#include <string>
+#include <iostream>
+#include "Q7.cpp"
 
 using namespace std;
 
 int main(){
+    Q7 q7;
 
+    int arr[] = {45, 2, 6, 68, 12, 10, 3, 2, 10, 21, 5};
+    int n = 11;
+    int* arr1 = nullptr;
+    int* arr2 = nullptr;
 
+    int i1 = 3;
+    int i2 = 7;
+
+    int n1 = i2 - i1 + 1;
+    int n2 = n - n1;
+
+    q7.splitArray(arr, 11, arr1, arr2, i1, i2);
+    q7.displayArr(arr1, n1);
+    q7.displayArr(arr2, n2);
+    q7.displayArr(arr, n);
+
+    delete[] arr1;
+    delete[] arr2;
     return 0;
 }

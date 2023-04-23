5e78f28879ff1e9fc99f2bbe140bf87865c57ac4
Musa Yiğit Yayla <91379566+Musa-Yigit-Yayla@users.noreply.github.com>
2023-04-23 11:35:39 +0300

    AddNode implemented

---
+++ DSA-LinkedListQuestions/AddNode.cpp
@@ -0,0 +1,50 @@
+
+class AddNode{
+public:
+    //Function to insert a new node at given position in doubly linked list.
+    void addNode(Node *head, int pos, int data){
+       // Your code here
+       Node* initialHead = head;
+       Node* tail = head;
+
+       Node* newNode = new Node(data);
+       int length = 0;
+       while(head != NULL) {
+           length++;
+           tail = head;
+           head = head->next;
+       }
+       //cout << length << " is length" << endl;
+       if(pos == 0 && head == NULL){
+           head = newNode;
+           return;
+       }
+       else if(pos == 0){ //&& head->next == NULL){
+            head = newNode;
+            newNode->next = initialHead;
+            initialHead->prev = newNode;
+            return;
+       }
+       else if(pos == length){
+           //insert at the very end right after the tail
+           //cout << "Tail data "<< tail->data << " tail index " << pos << endl;
+           //tail = tail->next;
+           tail->next = newNode;
+           newNode->prev = tail;
+           return;
+       }
+       Node* prevNode;
+       head = initialHead;
+       for(int i = 0; i <= pos && head->next != NULL; i++){
+           prevNode = head;
+           head = head->next;
+       }
+       //insert after prevNode, before head
+       prevNode->next = newNode;
+       head->prev = newNode;
+       newNode->prev = prevNode;
+       newNode->next = head;
+
+       head = initialHead;
+    }
+};

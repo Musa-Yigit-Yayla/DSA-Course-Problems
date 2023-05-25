5c32d0de4407be53cac9b595554e6b301bb1f351
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-05-25 19:40:59 +0300

    swapNodes function has been implemented
    Implemented for sll

--- DSA-LinkedListQuestions/SwapKthNodes.cpp
+++ DSA-LinkedListQuestions/SwapKthNodes.cpp
@@ -15,15 +15,17 @@ public:
             //cout << head->data << endl;
             head = head->next;
         }
-        Node* result = head;
+        Node* result = initialHead;
         //head = initialHead;
         return result;
 }
 
 //Function to swap Kth node from beginning and end in a linked list.
+//This method is not thoroughly functional, try and avoid using if that's not possible approach with attention.
 Node* swapkthnode(Node* head, int n, int k)
 {
-    // Your Code here
+    return this->swapNodes(head, k, n - k);
+    /*// Your Code here
     if(n <= 1){
         return head;
     }
@@ -46,18 +48,18 @@ Node* swapkthnode(Node* head, int n, int k)
 
     }
     else if(length - k != k){
-        Node* low = nodeAt(head, k - 1);
+        Node* low = nodeAt(head, k);
 
         Node* lowPrev = nullptr;
         Node* highNext = nullptr;
         if(k - 2 >= 0){
-            lowPrev = nodeAt(head, k - 2);
-            highNext = nodeAt(head, length - (k - 2));
+            lowPrev = nodeAt(head, k - 1);
+            highNext = nodeAt(head, length - (k + 1));
         }
-        Node* lowNext = nodeAt(head, k);
+        Node* lowNext = nodeAt(head, k + 1);
 
-        Node* high = nodeAt(head, length - (k - 1));
-        Node* highPrev = nodeAt(head, length - k);
+        Node* high = nodeAt(head, length - (k));
+        Node* highPrev = nodeAt(head, length - k - 1); //HIGHPREV VE LOWNEXTE BAK INDEXLERE
 
         if(lowPrev == NULL){
             highPrev->next = low;
@@ -72,7 +74,7 @@ Node* swapkthnode(Node* head, int n, int k)
             low->next = highNext;
         }
     }
-    return head;
+    return head;*/
 }
     void printList(Node* head){
         Node* currNode = head;
@@ -81,5 +83,126 @@ Node* swapkthnode(Node* head, int n, int k)
             currNode = currNode->next;
         }
         cout << endl;
+    }
+    //Swaps the nodes in a regular sll with the given indexes
+    //parameter indexing must belong to the subset of the range from 0 to length - 1
+    //Subsequently returns the head node
+    //If swap is unsuccessful also returns the head node
+    Node* swapNodes(Node* head, int i1, int i2){
+        //initially retrieve the node length
+        Node* currNode = head;
+        int length = 0;
+        while(currNode != NULL){
+            length++;
+            currNode = currNode->next;
+        }
+        //perform index validation
+        if(i1 >= length || i1 < 0 || i2 >= length || i2 < 0 || i1 == i2){
+            return head;
+        }
+        bool swapHead = false;
+        bool swapTail = false;
+        if(i1 == 0 || i2 == 0){
+            swapHead = true;
+        }
+        if(i1 == length - 1 || i2 == length - 1){
+            swapTail = true;
+        }
+        if(swapHead && !swapTail){
+            Node* headNode = head;
+            Node* otherNode = nullptr;
+            bool isI1Head  = false;
+            if(i1 == 0){
+                otherNode = this->nodeAt(head, i1);
+                isI1Head = true;
+            }
+            else{
+                otherNode = this->nodeAt(head, i2);
+            }
+            Node* otherPrev = nullptr;
+            Node* otherNext = nullptr;
+            if(isI1Head){
+                otherPrev = this->nodeAt(head, i1 - 1);
+                otherNext = this->nodeAt(head, i1 + 1);
+            }
+            else{
+                otherPrev = this->nodeAt(head, i2 - 1);
+                otherNext = this->nodeAt(head, i2 + 1);
+            }
+            Node* headNext = head->next;
+            otherNode->next = headNext;
+            otherPrev->next = head;
+            head->next = otherNode->next;
+            return otherNode;
+        }
+        else if(!swapHead && swapTail){
+            Node* tailNode = this->nodeAt(head, length - 1);
+            Node* tailPrev = this->nodeAt(head, length - 2);
+            Node* otherNode = nullptr;
+            Node* otherPrev = nullptr;
+            Node* otherNext = nullptr;
+
+            bool isI1Tail = false;
+            if(i1 == length - 1){
+                otherNode = this->nodeAt(head, i1);
+                otherPrev = this->nodeAt(head, i1 - 1);
+                otherNext = this->nodeAt(head, i1 + 1);
+                isI1Tail = true;
+            }
+            else{
+                otherNode = this->nodeAt(head, i2);
+                otherPrev = this->nodeAt(head, i2 - 1);
+                otherNext = this->nodeAt(head, i2 + 1);
+            }
+            otherPrev->next = tailNode;
+            tailNode->next = otherNext;
+            tailPrev->next = otherNode;
+            return head;
+        }
+        else if(swapHead && swapTail){
+            if(length == 2){
+                Node* headNext = head->next;
+                headNext->next = head;
+                head->next = nullptr;
+                return head;
+            }
+            Node* headNode = nullptr;
+            Node* headNext = nullptr;
+            Node* tailNode = nullptr;
+            Node* tailPrev = nullptr;
+            if(i1 == 0){
+                headNode = this->nodeAt(head, i1);
+                headNext = this->nodeAt(head, i1 + 1);
+                tailNode = this->nodeAt(head, length - 1);
+                tailPrev = this->nodeAt(head, length - 2);
+            }
+            else{//this block satisfies that i2 is the head node index
+                headNode = this->nodeAt(head, i2);
+                headNext = this->nodeAt(head, i2 + 1);
+                tailNode = this->nodeAt(head, length - 1);
+                tailPrev = this->nodeAt(head, length - 2);
+            }
+            tailNode->next = headNext;
+            tailNode->prev = headNode;
+            headNode->next = nullptr;
+            return tailNode;
+        }
+        else{//we are swapping some inner nodes, proceed accordingly
+            Node* n1 = this->nodeAt(head, i1);
+            Node* n1Prev = this->nodeAt(head, i1 - 1);
+            Node* n1Next = this->nodeAt(head, i1 + 1);
+            Node* n2 = this->nodeAt(head, i2);
+            Node* n2Prev = this->nodeAt(head, i2 -1);
+            Node* n2Next = this->nodeAt(head, i2 + 1);
+
+            //it would be the same procedure whether we have n1Next == n2Prev or not
+            n1Prev->next = n2;
+            n2->next = n1Next;
+
+            n2Prev->next = n1;
+            n1->next = n2Next;
+
+            return head;
+        }
     }
 };

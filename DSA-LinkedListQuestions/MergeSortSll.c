eac907accc91c981be679cd65e5a73a043f75080
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-06-23 19:48:47 +0300

    Implemented sortedMerge
    Exceeds time limit

---
+++ DSA-LinkedListQuestions/MergeSortSll.c
@@ -0,0 +1,55 @@
+
+Node* sortedMerge(Node* head1, Node* head2)
+{
+    // code here
+    int l1 = 0;
+    int l2 = 0;
+
+    Node* h1 = head1;
+    Node* h2 = head2;
+    while(h1 != NULL){
+        l1++;
+        h1 = h1->next;
+    }
+    while(h2 != NULL){
+        l2++;
+        h2 = h2->next;
+    }
+    //l1 is bigger or equal to l2
+    if(l1 < l2){
+        Node* temp = head2;
+        head2 = head1;
+        head1 = temp;
+
+        int templ = l2;
+        l2 = l1;
+        l1 = templ;
+    }
+    Node* prev1 = nullptr; // prev of curr1
+    Node* curr1 = head1;
+    Node* curr2 = head2;
+
+    //for(int i = 0; i < l1 + l2; i++){
+    while(curr1 != NULL && curr2 != NULL){
+        if(curr1->data >= curr2->data){
+            curr2->next = curr1;
+            if(prev1 != NULL){
+                prev1->next = curr2;
+            }
+            else{
+                prev1 = curr2;
+            }
+            curr2 = curr2->next;
+        }
+        else{
+            curr1 = curr1->next;
+        }
+    }
+    while(curr2 != NULL){
+        curr1->next = curr2;
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+    return head1;
+
+}

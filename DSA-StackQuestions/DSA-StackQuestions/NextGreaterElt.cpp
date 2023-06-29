0e05defb6c3353429f2049d66a8efe60f75cde1a
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-06-29 18:55:08 +0300

    NextLargerElt implemented
    nextLargerElement2 works fine, however the first approach needs debugging.

--- DSA-StackQuestions/DSA-StackQuestions/NextGreaterElt.cpp
+++ DSA-StackQuestions/DSA-StackQuestions/NextGreaterElt.cpp
@@ -3,32 +3,68 @@
 
 using namespace std;
 class NextGreaterElt{
-    public:
-    //Function to calculate the span of stock’s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
-    {
-       // Your code here
-       stack<int> results;
-       vector<int> returnValue;
+public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement2(vector<long long> arr, int n){
+        // Your code here
+        stack<long long> s;
+        stack<long long> results;
+        for(int i = n - 1; i >= 0; i--){
+            long long curr = this->calculate(s, arr.at(i));
+            results.push(curr);
+            s.push(arr.at(i));
+        }
 
-       for(int i =  0; i < n; i++){
-           returnValue.push_back(this->calculate(results, price[i]));
-           results.push(price[i]);
-       }
-       return returnValue;
-    }
-    int calculate(stack<int> results, int x){
-        int value = 1;
+        vector<long long> returnValue;
         while(!results.empty()){
-            if(results.top() <= x){
-                value++;
-                results.pop();
-            }
-            else{
+            returnValue.push_back(results.top());
+            results.pop();
+        }
+        return returnValue;
+    }
+    long long calculate(stack<long long> s, long long x){
+        int result = -1;
+        while(!s.empty()){
+            if(s.top() > x){
+                result = s.top();
                 break;
             }
+            else{
+                s.pop();
+            }
         }
-        return value;
+        return result;
+    }
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        stack<long long> s;
+        vector<long long> results;
+        int i = n - 1;
+        while(i >= 0){
+            if(s.empty()){
+                results.push_back(-1);
+                s.push(arr.at(i));
+                i--;
+                continue;
+            }
+            while(s.top() <= arr.at(i)){
+                s.pop();
+            }
+            if(!s.empty()){
+                results.push_back(s.top());
+            }
+            else{
+                results.push_back(-1);
+            }
+            s.push(arr.at(i));
+            i--;
+        }
+        //reverse results then return
+        for(int i = 0; i < results.size() / 2; i++){
+            long long temp = results.at(i);
+            results.at(i) = results.at(results.size() - 1 - i);
+            results.at(results.size() - 1 - i) = temp;
+        }
+        return results;
     }
 };
 

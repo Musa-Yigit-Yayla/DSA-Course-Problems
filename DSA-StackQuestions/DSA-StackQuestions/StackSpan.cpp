cc432cc67aafe534aae2dec035239ec30c6cd360
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-06-29 18:14:30 +0300

    StackSpan.cpp solved
    Solved, however exceeded time limit after having passed 101/159 testcases.

---
+++ DSA-StackQuestions/DSA-StackQuestions/StackSpan.cpp
@@ -0,0 +1,34 @@
+#include <stacj>
+
+using namespace std;
+
+class StackSpan{
+    public:
+    //Function to calculate the span of stock’s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       stack<int> results;
+       vector<int> returnValue;
+
+       for(int i =  0; i < n; i++){
+           returnValue.push_back(this->calculate(results, price[i]));
+           results.push(price[i]);
+       }
+       return returnValue;
+    }
+    int calculate(stack<int> results, int x){
+        int value = 1;
+        while(!results.empty()){
+            if(results.top() <= x){
+                value++;
+                results.pop();
+            }
+            else{
+                break;
+            }
+        }
+        return value;
+    }
+};
+

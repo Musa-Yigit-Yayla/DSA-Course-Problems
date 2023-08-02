b49ac8dc820839153f9b75012beba2252ff4227a
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-02 16:01:45 +0300

    Solved 2 contest questions
    Added only 1 since the other one used some methods that we have already implemented and was comprised of only a few lines.

--- TreeQuestions/TreeOperations.cpp
+++ TreeQuestions/TreeOperations.cpp
@@ -115,4 +115,55 @@ public:
         }
         return ceil;
     }
+    vector<int> inOrder(Node* root) {
+      vector<int> result;
+      if(root != NULL){
+          vector<int> leftTree = inOrder(root->left);
+          result = leftTree;
+          result.push_back(root->data);
+          vector<int> rightTree = inOrder(root->right);
+          //result.insert(result.end(), leftTree.begin(), leftTree.end());
+          result.insert(result.end(), rightTree.begin(), rightTree.end());
+      }
+      return result;
+    }
+    Node* getNodeByValue(Node* root, const int data){
+        if(root != NULL){
+            if(root->data == data){
+                return root;
+            }
+            else{
+                Node* leftSubtree = getNodeByValue(root->left, data);
+                Node* rightSubtree = getNodeByValue(root->right, data);
+                if(leftSubtree != NULL){
+                    return leftSubtree;
+                }
+                else{
+                    return rightSubtree;
+                }
+            }
+        }
+        return nullptr;
+    }
+    int median(Node *node,int k){
+        //Your code here
+        vector<int> vec = inOrder(node);
+        if(k > vec.size()){
+            return -1;
+        }
+        else{
+            Node* currRoot = getNodeByValue(node, vec.at(k - 1));
+            vector<int> subVec = inOrder(currRoot);
+            int result;
+            if(subVec.size() % 2 == 0){
+                int i1 = subVec.size() / 2 - 1;
+                int i2 = i1 + 1;
+                result = (subVec.at(i1) + subVec.at(i2)) / 2;
+            }
+            else{
+                result = subVec.at(subVec.size() / 2);
+            }
+            return result;
+        }
+    }
 };

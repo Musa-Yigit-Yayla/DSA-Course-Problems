1acc54294a484d434a58baf57437e6ad51c4533e
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-12 18:17:53 +0300

    Alternated implementation slightly

--- DSA-GraphProblems/RottenOranges.cpp
+++ DSA-GraphProblems/RottenOranges.cpp
@@ -1,9 +1,10 @@
 #include <vector>
 
 using namespace std;
-class Solution{
+class RottenOranges{
 private:
     vector<vector<vector<int>>> adjList; //contains adjacency list disregarding empty cells
+    int columnCount;
     //the 3d vector data field contains a 2d vector which is the adjacency list of every non empty cell
     //which ultimately contains the index of the element at the innermost vector as the 0th element
     //and the element type in the first index
@@ -14,7 +15,7 @@ public:
         // Code here
         //0 for empty cell 1 for fresh orange 2 for rotten orange
         //initialize the adjList
-        const int columnCount = grid.at(0).size();
+        this->columnCount = grid.at(0).size();
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < columnCount; j++){
                 int currIndex = columnCount * i + j;
@@ -41,7 +42,7 @@ public:
             int currIndex = currAdj.at(0).at(0);
             int currValue = currAdj.at(0).at(1);
             if(currValue == 1){
-                this->findTime(currIndex, -1, minTime, 0);
+                this->findTime(currIndex, -1, minTime, 0, visit);
                 int currResult = minTime[currIndex];
                 if(currResult > returnValue){
                     returnValue = currResult;
@@ -51,15 +52,17 @@ public:
         return returnValue;
     }
     //Finds the minimum time required for a fresh orange to rot
-    void findTime(const int startVertex, int currVertex, int minTime[], int currTime){
+    void findTime(const int startVertex, int currVertex, int minTime[], int currTime, bool visited[]){
+        //cout << "Visiting: (" << to_string(currVertex / columnCount) << ", " << to_string(currVertex % columnCount) << "), currTime: " << to_string(currTime) << endl;
+        visited[currVertex] = true;
         if(currVertex == -1){
             //start of the recursion
             vector<vector<int>> currAdj = this->adjList.at(startVertex);
             for(int i = 0; i < currAdj.size(); i++){
                 int currAdjValue = currAdj.at(i).at(1);
                 int currAdjIndex = currAdj.at(i).at(0);
-                if(currAdjValue == 1){
-                    findTime(startVertex, currAdjIndex, minTime, currTime + 1);
+                if(currAdjValue == 1 && !visited[currAdjIndex]){
+                    findTime(startVertex, currAdjIndex, minTime, currTime + 1, visited);
                 }
                 else if(currAdjValue == 2){
                     if(minTime[startVertex] > currTime + 1){
@@ -72,11 +75,11 @@ public:
             if(currVertex == 1){
                 vector<vector<int>> currAdj = this->adjList.at(currVertex);
                 for(int i = 0; i < currAdj.size(); i++){
-                    int currAdjValue = currAdj.at(0).at(1);
-                    int currAdjIndex = currAdj.at(0).at(0);
-                    if(currAdjValue == 1 && currAdjIndex != startVertex){ //second logic allows us to avoid
+                    int currAdjValue = currAdj.at(i).at(1);
+                    int currAdjIndex = currAdj.at(i).at(0);
+                    if(currAdjValue == 1 && currAdjIndex != startVertex && !visited[currAdjIndex]){ //second logic allows us to avoid
                     //infinite recursion if there exists a cycle of 1s in our graph
-                        findTime(startVertex, currAdjIndex, minTime, currTime + 1);
+                        findTime(startVertex, currAdjIndex, minTime, currTime + 1, visited);
                     }
                     else if(currAdjValue == 2){
                         if(minTime[startVertex] > currTime + 1){
@@ -91,5 +94,6 @@ public:
                 }
             }
         }
+        visited[startVertex] = false; //reset for bakctracking
     }
 };

004a5577f4663f1f143d27654dd2deeeb47b45eb
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-27 22:10:01 +0300

    Additional implementation

--- TreeQuestions/MaxHeapSort.cpp
+++ TreeQuestions/MaxHeapSort.cpp
@@ -11,10 +11,49 @@ private:
     int n;
 
 public:
+    #include <cstddef>
+
+class Solution{
+private:
+    int* arr = nullptr;
+    int currSize = 0;
+    int n;
+
+public:
     //Heapify function to maintain heap property.
+    //This function processes the given array pointer hence you can use it for other arrays too
     void heapify(int arr[], int n, int i)
     {
       // Your Code Here
+      //We should call the heapify when we have a semi-heap
+      //trickle the first node down by swapping it with its larger child and continue the process
+      int currIndex = 0;
+      int largerChild = INT_MIN;
+      int largerIndex = -1;
+      if(2 * currIndex + 1 < n){
+          largerIndex = 2 * currIndex + 1;
+          largerChild = arr[largerIndex];
+      }
+      if(2 * currIndex + 2 < n && arr[2 * currIndex + 2] > largerChild){
+          largerIndex = 2 * currIndex + 2;
+          largerChild = arr[largerIndex];
+      }
+      while(currIndex < n && arr[currIndex] < largerChild){
+          //swap with the larger child then update the indexes
+          int temp = arr[currIndex];
+          arr[currIndex] = largerChild;
+          arr[largerIndex] = temp;
+
+          currIndex = largerIndex;
+          if(2 * currIndex + 1 < n){
+            largerIndex = 2 * currIndex + 1;
+            largerChild = arr[largerIndex];
+          }
+          if (2 * currIndex + 2 < n && arr[2 * currIndex + 2] > arr[largerIndex]) {
+                largerIndex = 2 * currIndex + 2;
+                largerChild = arr[2 * currIndex + 2];
+        }
+      }
     }
 
     //Function to build a Heap from array.
@@ -29,6 +68,7 @@ public:
         this->n = n;
         for(int i = 0; i < this->n; i++){
             this->insertKey(arr[i], i);
+            this->currSize++;
         }
     }
 
@@ -40,9 +80,9 @@ public:
         this->buildHeap(arr, n);
 
         for(int i = n - 1; i >= 0; i--){
-            int currMax =
+            int currMax = this->popMax();
+            arr[i] = currMax;
         }
-
     }
 private: //private helper functions
     void insertKey(int key, int currSize){
@@ -62,8 +102,22 @@ private: //private helper functions
             }
         }
     }
+    //Pops and returns the max element, once it's popped we will have a semi heap remaining
     int popMax(){
-
+        int returnValue = -1;
+        if(this->currSize != 0){
+            returnValue = this->arr[0];
+            if(this->currSize > 1){
+                int lastElt = this->arr[this->currSize - 1];
+                this->arr[0] = lastElt;
+                this->arr[this->currSize - 1] = 0;
+            }
+            else{
+                this->arr[this->currSize - 1] = 0;
+            }
+            this->currSize--;
+        }
+        return returnValue;
     }
 
 };

5e2a646d8c819434268ab64fd3ac23a84a290869
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-29 00:40:40 +0300

    Implementation is about to be completed

--- DSA-GraphProblems/NumberOfIslands.cpp
+++ DSA-GraphProblems/NumberOfIslands.cpp
@@ -2,8 +2,11 @@
 
 using namespace std;
 class NumberOfIslands{
-  private:
+   private:
     vector<vector<int>> adjList;
+    int rowLength;
+    int gridLength;
+    vector<vector<char>>& grid;
   public:
 
     const int charValue0 = 48; //in decimal
@@ -14,35 +17,93 @@ class NumberOfIslands{
         // Code here
         //initialize the adjList so that we have it full of empty vectors with number of grid elements
         int gridElementCount = grid.size() * grid.at(0).size();
-        for(int i = 0; i < gridElementCount; i++){
+        for(int i = 0; i < gridElementCount + 1; i++){
             vector<int> vec;
             this->adjList.push_back(vec);
         }
+        this->grid = grid;
         //create a graph which is undirected and unweighted using adj list
         //we must also count the number of vertices and give them values according to their grid pos
-        int rowLength = grid.at(0).size();
-        int gridLength = grid.size();
+        this->rowLength = grid.at(0).size();
+        this->gridLength = grid.size();
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < rowLength; j++){
-                int currLabel = this->getLabel(i, j, rowLength, gridLength, grid);
+                int currLabel = this->getLabel(i, j, rowLength);
                 //retrieve the adjacent nodes with their label
                 vector<int> adjNodes = this->getAdjNodes(i, j, rowLength, gridLength, grid);
                 this->adjList.at(currLabel) = adjNodes;
             }
         }
 
+        int result = 0;
         //subsequently we must count the number of connected graphs and return it as the result
         //to do this we need to traverse each and every graph node, once we cannot go to anywhere
         //we must traverse other nodes which are not connected to the graph that we have just traversed
-
-
+        bool visit[gridElementCount];
+        for(int i = 0; i < gridElementCount; i++){
+            visit[i] = false;
+        }
+        int falseIndex = this->getFalseIndex(visit, gridElementCount);
+        while(falseIndex != -1){
+            int currLabel = falseIndex + 1; //1 based indexing for the vertices
+            this->dfs(currLabel, visit, gridElementCount);
+            //increment the result which represents number of connected graphs by one
+            result++;
+            falseIndex = this->getFalseIndex(visit, gridElementCount);
+        }
+        return result;
         //notice that we regard diagonally adjacent nodes connected as well
+    }
+    //Iterative dfs approach
+    //notice that visit is 0 based indexing whereas currLabel is 1 based indexing
+    void dfs(int currLabel, bool visit[], int gridElementCount){
+        stack<int> s;
+        //mark currLabel as visited
+        visit[currLabel - 1] = true;
+        s.push(currLabel);
+
+        while(!s.empty()){
+            bool noUnvisitedAdj = true;
+            int currLabel = s.top();
+            int verticeRow = this->getVerticeRow(currLabel);
+            int verticeColumn = this->getVerticeColumn(currLabel);//!!!MIGHT BE PROBLEMATIC PAY ATTENTION
+            vector<int> currAdj = this->getAdjNodes(verticeRow, verticeColumn, rowLength, gridLength, grid);
+
+            for(int i = 0; i < currAdj.size(); i++){
+                int currAdj = currAdj.at(i);
+                if(visit[currAdj]){
+                    noUnvisitedAdj = false;
+                    break;
+                }
+            }
+
+            if(noUnvisitedAdj){
+                //backtrack
+                s.pop();
+            }
+            else{
+                //select an unvisited vertex at currAdj push it onto the stack and mark it as visited
+                int nextVertex = currAdj.at(0);
+                s.push(nextVertex);
+                visit[nextVertex] = true;
+            }
+
+        }
+
     }
     int getLabel(int row, int column, const int rowLength){
         return row * rowLength + column + 1; //we start counting from 1
     }
+    int getVerticeRow(int label){
+        int verticeRow = currLabel / rowLength;
+        return verticeRow;
+    }
+    int getVerticeColumn(int label){
+        int verticeColumn = currLabel % rowLength - 1;
+        return verticeColumn;
+    }
     //Returns the adjacent nodes if any
-    vector<int> getAdjNodes(int row, int column, const int rowLength, gridLength, vector<vector<char>>& grid){
+    vector<int> getAdjNodes(int row, int column, const int rowLength, const int gridLength, vector<vector<char>>& grid){
         vector<int> result;
         //start from top left
         int currRow = row - 1;
@@ -103,6 +164,18 @@ class NumberOfIslands{
                 if(ch == charValue1){
                     result.push_back(this->getLabel(currRow, currColumn, rowLength));
                 }
+            }
+        }
+        return result;
+    }
+    //returns the index of the first occurence of visit, notice that this is 0 based indexing
+    //returns -1 if each element evaluates to true
+    int getFalseIndex(bool visit[], int length){
+        int result = -1;
+        for(int i = 0; i < length; i++){
+            if(!visit[i]){
+                result = i;
+                break;
             }
         }
         return result;

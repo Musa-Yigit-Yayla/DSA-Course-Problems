21f324fc2f5f792b3470cd387b2cb81ead0dea85
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-29 18:58:24 +0300

    FindPath Improvements
    Improved the solution so that it now can be compiled. However, there are run-time errors.

--- DSA-GraphProblems/FindPath.cpp
+++ DSA-GraphProblems/FindPath.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 class FindPath{
 private:
-    int** adjMatrix = nullptr;
-    int rowCount;
-    int columnCount;
-    int matrixSize;
+    int** adjMatrix = nullptr; //in adjMatrix 1 implies adjacency
+    int rowCount = 0;
+    int columnCount = 0;
+    int matrixSize = 0;
 
 public:
     //Function to find whether a path exists from the source to destination.
@@ -26,9 +26,28 @@ public:
             }
             delete[] adjMatrix;
         }
-        this->initializeAdjMatrix(grid);
+        this->rowCount = grid.size();
+        this->columnCount = grid.at(0).size();
+        this->matrixSize = this->rowCount * this->columnCount;
+        const int matrixLength = this->matrixSize;
+        this->initializeAdjMatrix(grid, matrixLength);
         //notice that we label the nodes from index 0, first vertex is labeled as 0
 
+        //retrieve tar and dest labels respectively with a single function call
+        vector<int> positions = this->retrieveTarDestLabels(grid);
+        bool pathFound = false;
+        if(positions.size() != 0){
+            bool visit[this->matrixSize];
+            //initialize the visit array to false
+            for(int i = 0; i <  this->matrixSize; i++){
+                visit[i] = false;
+            }
+            int startLabel = positions.at(0);
+            int destLabel = positions.at(1);
+            this->bfs(grid, startLabel, visit);
+            pathFound = visit[destLabel];
+        }
+        return pathFound;
     }
     int getLabel(int row, int column){
         return row * this->columnCount + column;
@@ -41,20 +60,54 @@ public:
     }
 private:
     //after we execute our bfs starting from the startlabel we will mark every reachable vertex
-    void bfs(vector<vector<int>>& visit, int startLabel){
+    void bfs(vector<vector<int>>& grid, int startLabel, bool visit[]){
+        int vertexCount = this->matrixSize; //visit length is represented by vertexCount
+        int startRow = this->getLabelRow(startLabel);
+        int startColumn = this->getLabelColumn(startColumn);
 
+        queue<int> q;
+        //mark root as visited and enqueue it
+        q.push(startLabel);
+        visit[startLabel] = true;
+        while(!q.empty()){
+            //retrieve each and every unvisited adjacent vertex of the top of the queue
+            //enqueue them, then continue
+            //if no unvisited adjacent vertex exists then dequeue
+            int topElt = q.front();
+            vector<int> currAdjList = this->getAdjLabels(grid, topElt);
+            //remove the unvisited adjacents from the list
+            for(int i = 0; i < currAdjList.size(); i++){
+                int currAdjLabel = currAdjList.at(i);
+                if(this->adjMatrix[topElt][currAdjLabel] != 1){
+                    //erase since there is no adjacency
+                    vector<int>::iterator it = currAdjList.begin() + i;
+                    currAdjList.erase(it);
+                }
+            }
+            if(currAdjList.size() != 0){
+                //enqueue each and every unvisited adjacent node and mark them as visited
+                for(int adjLabel: currAdjList){
+                    q.push(adjLabel);
+                    visit[adjLabel] = true;
+                }
+            }
+            else{
+                //backtrack
+                q.pop();
+            }
+        }
     }
     //Invoke when adjMatrix has been deallocated so as to avoid memory leak
-    void initializeAdjMatrix(vector<vector<int>>& grid){
+    void initializeAdjMatrix(vector<vector<int>>& grid, const int matrixLength){
         //notice we cannot move diagonally and we only consider direct adjacency
-        this->rowCount = grid.size();
-        this->columnCount = grid.at(0).size;
-        this->matrixSize = this->rowCount * this->columnCount;
 
-        this->adjMatrix = new int[this->matrixSize][this->matrixSize];
+
+        this->adjMatrix = new int*[matrixLength];
+
 
         //initialize each and every matrix element to infinity initially
         for(int i = 0; i < this->matrixSize; i++){
+            this->adjMatrix[i] = new int[matrixLength];
             for(int j = 0; j < this->matrixSize; j++){
                 this->adjMatrix[i][j] = INT_MAX;
             }
@@ -66,10 +119,19 @@ private:
             int labelRow = this->getLabelRow(currLabel);
             int labelColumn = this->getLabelColumn(currLabel);
             if(grid.at(labelRow).at(labelColumn) != 0){
-                this->getAdjLabels(grid, label);
+                vector<int> adjVertices = this->getAdjLabels(grid, currLabel);
+                for(int adjLabel: adjVertices){
+                    int adjRow = this->getLabelRow(adjLabel);
+                    int adjColumn = this->getLabelColumn(adjColumn);
+
+                    if(grid.at(adjRow).at(adjColumn) != 0){
+                        this->adjMatrix[currLabel][adjLabel] = 1;
+                    }
+                }
             }
         }
     }
+    //we don't check whether adjacent labels are walls or they are visited
     vector<int> getAdjLabels(vector<vector<int>>& grid, int label){
         int labelRow = this->getLabelRow(label);
         int labelColumn = this->getLabelColumn(label);
@@ -83,31 +145,54 @@ private:
         int nextLabel;
 
         if(currRow >= 0){
-            nextLabel = this->getLabel(currRow, curreColumn);
+            nextLabel = this->getLabel(currRow, currColumn);
             result.push_back(nextLabel);
         }
         //check left
         currRow =  labelRow;
         currColumn = labelColumn - 1;
         if(currColumn  >= 0){
-            nextLabel = this->getLabel(currRow, curreColumn);
+            nextLabel = this->getLabel(currRow, currColumn);
             result.push_back(nextLabel);
         }
         //check bottom
         currRow =  labelRow + 1;
         currColumn = labelColumn;
         if(currRow < this->rowCount){
-            nextLabel = this->getLabel(currRow, curreColumn);
+            nextLabel = this->getLabel(currRow, currColumn);
             result.push_back(nextLabel);
         }
         //check right
         currRow =  labelRow;
         currColumn = labelColumn + 1;
         if(currColumn < this->columnCount){
-            nextLabel = this->getLabel(currRow, curreColumn);
+            nextLabel = this->getLabel(currRow, currColumn);
             result.push_back(nextLabel);
         }
         return result;
     }
-
+    //Return empty vector if one or more of the elements do not exist
+    vector<int> retrieveTarDestLabels(vector<vector<int>>& grid){
+        //1 means tar, 2 means dest
+        vector<int> result;
+        int tar = -1;
+        int dest = -1;
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid.at(i).size(); j++){
+                int curr = grid.at(i).at(j);
+                if(curr == 1){
+                    tar = this->getLabel(i, j);
+                }
+                else if(curr == 2){
+                    dest = this->getLabel(i, j);
+                }
+                if(tar != -1 && dest != -1){
+                    result.push_back(tar);
+                    result.push_back(dest);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
 };

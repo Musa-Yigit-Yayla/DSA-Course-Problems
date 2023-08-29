6dc0aafed1b477f4da0b9c5e07c1ba74fbf9bdc5
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-08-29 15:37:32 +0300

    NOI Solved
    Apparently we have solved it thoroughly in our local.

--- DSA-GraphProblems/main.cpp
+++ DSA-GraphProblems/main.cpp
@@ -6,7 +6,16 @@ using namespace std;
 int main()
 {
     NumberOfIslands noi;
-    vector<vector<char>> input{{{0,1},{1,0},{1,1},{1,0}}};
+    //vector<vector<char>> input{{{0,1},{1,0},{1,1},{1,0}}};
+    vector<vector<char>> input{{0, 1, 0},
+                                {0, 1, 0},
+                                {0, 0, 0},
+                                {1, 1, 0},
+                                {1, 0, 1},
+                                {0, 1, 1},
+                                {1, 1, 1},
+                                {0, 1, 1},
+                                {1, 0, 1}};
     int result = noi.numIslands(input);
     cout << result << endl;
     return 0;

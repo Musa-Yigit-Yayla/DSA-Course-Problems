bb839174e02f8ae688e526f5dd44f8f1f3811be8
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-04-21 23:51:52 +0300

    Yields heap buffer overflow
    You may try to run in the local machine first.

--- DSA-DynamicProgramming/LongestPalindromicSubstring.cpp
+++ DSA-DynamicProgramming/LongestPalindromicSubstring.cpp
@@ -6,12 +6,15 @@ public:
         //we can use tabulazation as a bottom up approach
         //we can use a 2d bool array to store the subproblem results
         string result = "";
-        int tableLength = s.size() * (s.size() + 1) / 2;
+        //int tableLength = s.size() * (s.size() + 1) / 2;
+        int tempLength = s.size();
+        const int arrLength = *const_cast<const int*>(&tempLength);
 
         //first initialize the contents
-        bool** arr = new bool[s.size()][s.size()]; //array contains rowIndex as the start index and columnIndex as the end index (inclusive)
+        bool** arr = new bool*[arrLength]; //array contains rowIndex as the start index and columnIndex as the end index (inclusive)
         //of a substring. The result specifies whether the designated substring is palindromic
         for(int i = 0; i < s.size(); i++){
+            arr[i] = new bool[arrLength];
             for(int j = 0; j < s.size(); j++){
                 arr[i][j] = false;
             }
@@ -22,7 +25,7 @@ public:
         }
         //also set up the length 2 substring entries if they are palindromic directly for increased efficiency
         for(int i = 1; i < s.size(); i++){
-            if(this->isPalindrome(s.substr(i - 1), i)){
+            if(this->isPalindrome(s.substr(i - 1, 2))){
                 arr[i - 1][i] = true;
             }
         }
@@ -47,10 +50,20 @@ public:
             for(int j = 4; j >= i; j--){
                 if(arr[i][j] == true){
                     //we have a palindrome, check if it is of larger length than current result
-                    string currStr = s.substr(i)
+                    int length = j - i + 1;
+                    string currStr = s.substr(i, length);
+                    if(currStr.size() > result.size()){
+                        result = currStr;
+                    }
                 }
             }
         }
+        //deallocate the 2d array and return the result;
+        for(int i = 0; i < s.size(); i++){
+            delete arr[i];
+        }
+        delete[] arr;
+        return result;
 
         /*for(int i = 1; i < s.size(); i++){
             for(int j = 0; j < s.size() - i + 1){

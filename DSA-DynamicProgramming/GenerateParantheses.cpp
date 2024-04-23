c20244f0eb3b097a706a96db2489bd4f4ada5591
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-04-23 16:44:40 +0300

    GP initial implementation
    Currently generates index out of bounds error.

---
+++ DSA-DynamicProgramming/GenerateParantheses.cpp
@@ -0,0 +1,80 @@
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class GenerateParantheses{
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        unordered_map<string, bool> tableDP;
+        //we will proceed with a bottom up approach
+        tableDP.insert(make_pair("()", true));
+        tableDP.insert(make_pair("()()", true));
+        tableDP.insert(make_pair("(())", true));
+        //we can start our calculations from n 3
+        for(int i = 3; i <= n; i++){
+            //fetch all permutations of parantheses strings of length i
+            //first retrieve parantheses strings of length i - 1 from tableDP and store them in a vector
+            vector<string> prevElts;
+            int prevLength = (i - 1) * 2;
+            for(auto& it: tableDP){
+                if(it.first.size() == prevLength){
+                    prevElts.push_back(it.first);
+                }
+            }
+            unordered_set<string> currElts = this->permute(prevElts);
+
+            for(unordered_set<string>::iterator it = currElts.begin(); it != currElts.end(); it++){
+               if(i == n){
+                    //simply fill the result vector since this is the desired level
+                    result.push_back(*it);
+               }
+               else{//track intermediate results
+                    tableDP.insert(make_pair(*it, true));
+               }
+            }
+
+        }
+        return result;
+    }
+    //Validates whether a given string consists of well formed parantheses
+    bool validate(string str){
+        stack<char> s;
+        for(int i = 0; i < str.size(); i++){
+            char curr = str.at(i);
+            if(curr == ')'){
+                //if the top element is '(', then pop it and we have a match
+                if(s.empty()){
+                    return false; //not possible to be valid
+                }
+                else{
+                    if(s.top() == '('){
+                        s.pop();
+                    }
+                }
+            }
+            else if(curr == '('){
+                s.push(curr);
+            }
+        }
+        return s.empty(); //if the stack is empty, we have well formed parantheses in our string
+    }
+    //prevElements are the valid strings containing n - 1 matching parantheses
+    unordered_set<string> permute(vector<string>& prevElements){
+        unordered_set<string> result;
+        for(string s: prevElements){
+            for(int i = 0; i <= s.size(); i++){
+                string newStr = s.substr(0, i) + '(' + s.substr(i);
+                for(int j = i + 1; j <= newStr.size(); j++){
+                    newStr = newStr.substr(0, j) + ')' + s.substr(j);
+                    result.insert(newStr);
+                }
+            }
+        }
+        return result;
+    }
+};
